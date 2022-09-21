0460eaae3365d857c3f299b1b9f6c98ae4010944
windo <windo@DESKTOP-IALE1EM>
2022-09-21 22:14:34 +0900

    Hello java!

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,13 @@
+package sec03.exam01;
+
+// 2022. 9. 16. 20:28 김철희
+
+public class Hello {   // 주석 사용해보기.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello, Java");
+			
+	} // end of main
+
+} // end of class
